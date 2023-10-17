9413fc8f0eede99c8788f7c02bdeee886244865b
saikumar <saikumarsangi4433@gmail.com>
2023-10-17 19:20:20 +0530

    project1

---
+++ src/main/java/com/example/javaprojectdemo/JavaprojectdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprojectdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaprojectdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaprojectdemo/JavaprojectdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojectdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
